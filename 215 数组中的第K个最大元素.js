44a4f360470ffb8bc0ebc461cbba4e3ddeb616c1
abu <abusuper@outlook.com>
2020-02-29 20:36:35 +0800

    Update 215 数组中的第K个最大元素.js

--- 215 数组中的第K个最大元素.js
+++ 215 数组中的第K个最大元素.js
@@ -4,17 +4,7 @@
  * @return {number}
  */
 var findKthLargest = function (nums, k) {
-    let res = new Array(k)
-    // res[0] = nums[0]
-    // for (let i = 1; i < nums.length; i++) {
-    //     res.push(nums[i])
-    //     res.sort()
-    //     res.pop
-    // }
-    nums.sort(function (a, b) {
-        return b - a
-    })
-    return nums
+
 };
 
 console.log(findKthLargest([3, 2, 1, 5, 6, 4], 2))
