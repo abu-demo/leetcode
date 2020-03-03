00f08060c29d1fdd3ddc0e6a41183ebe72dfe426
abu <abusuper@outlook.com>
2020-03-03 22:49:06 +0800

    Create 1342.将数字变成0的操作次数.js

---
+++ 1342.将数字变成0的操作次数.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var numberOfSteps = function (num) {
+    let count = 0
+
+    while (num != 0) {
+        count++
+
+        num = num % 2 == 0 ? num / 2 : num - 1
+    }
+
+    console.log(count)
+};
+
+numberOfSteps(12)
