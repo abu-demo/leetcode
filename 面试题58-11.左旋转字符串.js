46a81dcea0c79507a80da6ed3b76bac3d4789cf9
abu <abusuper@outlook.com>
2020-03-03 22:43:16 +0800

    Create 面试题58-11.左旋转字符串.js

---
+++ 面试题58-11.左旋转字符串.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @param {number} n
+ * @return {string}
+ */
+var reverseLeftWords = function (s, n) {
+    return s.slice(n, s.length) + s.slice(0, n)
+};
+
+console.log(reverseLeftWords('abcdefg', 2))
