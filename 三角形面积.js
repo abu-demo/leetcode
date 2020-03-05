54258958ec946851c9fff9de8a8f05c7a7c7fd05
abu <abusuper@outlook.com>
2020-03-05 16:45:18 +0800

    Create 三角形面积.js

---
+++ 三角形面积.js
@@ -0,0 +1,11 @@
+let x1 = 2,
+    y1 = 3,
+    x2 = 6,
+    y2 = 4,
+    x3 = 5,
+    y3 = 7;
+let X1 = x2 - x1,
+    Y1 = y2 - y1,
+    X2 = x3 - x1,
+    Y2 = y3 - y1;
+console.log((X1 * Y2 - X2 * Y1) / 2);
