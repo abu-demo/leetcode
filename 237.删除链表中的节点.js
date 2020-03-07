f1c5d2540827d17420cb5f4541d70b3241c14acd
abu <abusuper@outlook.com>
2020-03-07 15:21:47 +0800

    Create 237.删除链表中的节点.js

---
+++ 237.删除链表中的节点.js
@@ -0,0 +1,16 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+    // 给的node节点就是 要被删除的节点
+    node.val = node.next.val
+    node.next = node.next.next
+};
