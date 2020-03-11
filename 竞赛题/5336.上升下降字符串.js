b5c058492c46a83affd1001aa30c2b8ab77e32e6
abu <abusuper@outlook.com>
2020-03-11 21:15:07 +0800

    Create 5336.上升下降字符串.js

---
+++ 竞赛题/5336.上升下降字符串.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortString = function (s) {
+    let res = ''
+
+    let arr = s.split('').sort()
+
+    let min = 'z'
+
+    for (e of arr) {
+        e < z ? min = e
+    }
+
+
+
+
+    console.log(arr)
+};
+
+console.log(sortString("leetcode"))
